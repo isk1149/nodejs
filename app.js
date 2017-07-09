547253c335c4c1fbfc9783028ed6b474865980f8
isk1149 <isk1149@naver.com>
2017-07-09 20:57:44 +0900

    study 0709

---
+++ app.js
@@ -0,0 +1,142 @@
+var express = require('express');
+var app = express(); // express를 사용하려면 이 두줄까지는 형식적으로 작성.
+var bodyParser = require('body-parser');//post방식을 사용하기 위한 모듈
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static('public'));//정적인 파일은 public폴더에 잇다.
+app.set('view engine', 'jade'); //jade와 express를 연결.
+app.set('views', './views'); //템플릿이 있는 폴더를 express에 알려주는 코드. 관습적으로 views라는 이름으로 정했다.
+
+/*express에는 listen이라는 메소드가 있는데 포트번호를 지정해주면 리스닝한다*/
+app.listen(3000, ()=>{
+  console.log("connected 3000 port");
+})
+
+/*사용자가 웹서버에 접속할때는 get방식 or post방식이 잇다.
+보통 url을 치고 들어오는 경우는 get방식*/
+
+// '/'은 홈으로 들어온 경우
+//req는 사용자가 요청한 것과 관련된 정보를 담은 객체
+//res는 응답에 대한 객체.
+//get함수는 라우터의 기능이다.
+app.get('/', (req, res)=>{
+  res.send('welcome to home');
+});
+
+app.get('/login', (req, res)=>{
+  res.send('<h1>login page</h1>');
+})
+
+//나름 동적으로 만든 페이지인데
+app.get('/dynamic', function(req, res){
+  var lis = '';
+  for(var i=0; i<5; i++){
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      Hello, Dynamic!
+        <ul>
+          ${lis}
+        </ul>
+        ${time}
+    </body>
+  </html>`;
+  res.send(output);
+ });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//semantic url에서는 req.query 대신 req.params
+app.get('/topic/:id', function(req, res){
+  var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic/0">JavaScript</a><br>
+  <a href="/topic/1">Nodejs</a><br>
+  <a href="/topic/2">Express</a><br><br>
+  ${topics[req.params.id]}
+  `;
+  res.send(output);
+});
+
+app.get('/topic/:id/:mode', function(req, res){
+  res.send(req.params.id+','+req.params.mode)
+});
+
+
+//query string 방식
+//req.query.id => request객체가 가지는 query객체의 id값
+//즉 사용자가 url을 통해 입력한 주소가 req로 전달되고 그 중 query객체의 id값을 받는다.??
+/*
+app.get('/topic', function(req, res){
+  var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br><br>
+  ${topics[req.query.id]}
+  `
+  res.send(output);
+})
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/form', function(req, res){
+   res.render('form');
+ });
+
+/*
+app.get('/form_receiver', function(req, res){
+   var title = req.query.title;
+   var description = req.query.description;
+   res.send(title+','+description);
+ });*/
+
+
+//post방식은 req.query가 아니라 req.body를 쓴다.
+//기본적으로 post방식으로 전달되는 데이터는 undefined상태이다. 이용하려면 body-parser설치.
+ app.post('/form_receiver', function(req, res){
+   var title = req.body.title;
+   var description = req.body.description;
+   res.send(title+','+description);
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ app.get('/template', (req, res)=>{
+   //템플릿을 사용할 때는  render함수 사용.
+   ///template를 통해 들어온 사용자에게 temp.jade라는 파일을 렌더링해서 화면에 띄어준다.
+   //render는 템플릿 함수이므로 자동적으로 views폴더에 있는 temp.jade를 찾는다.
+   res.render('temp', {time_value:Date(), title_value:'Jadess'});
+ });
+*/
+
+/*
+ app.get('/flowers', (req, res)=>{
+   res.send('<img src="/flower.jpg">');
+ })
+ */
---
+++ app_file.js
@@ -0,0 +1,105 @@
+var express = require('express');
+ var bodyParser = require('body-parser');
+ /*var multer = require('multer');
+ var _storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+     cb(null, 'uploads/')
+   },
+   filename: function (req, file, cb) {
+     cb(null, file.originalname);
+   }
+ })
+ var upload = multer({ storage: _storage })*/
+ var fs = require('fs');
+ var app = express();
+ app.use(bodyParser.urlencoded({ extended: false }));
+ app.locals.pretty = true; // 웹브라우저상에서 html구문 이쁘게 해줌
+ //app.use('/user', express.static('uploads'));
+ app.set('views', './views_file');
+ app.set('view engine', 'jade');
+ /*app.get('/upload', function(req, res){
+   res.render('upload');
+ });
+ app.post('/upload', upload.single('userfile'), function(req, res){
+   res.send('Uploaded : '+req.file.filename);
+ });*/
+
+
+ /*
+ app.get(['/topic', '/topic/:id'], function(req, res){
+   fs.readdir('data', function(err, files){
+     if(err){
+       console.log(err);
+       res.status(500).send('Internal Server Error');
+     }
+     var id = req.params.id;
+     if(id){
+       // id값이 있을 때
+       fs.readFile('data/'+id, 'utf8', function(err, data){
+         if(err){
+           console.log(err);
+           res.status(500).send('Internal Server Error');
+         }
+         res.render('view', {topics:files, title:id, description:data});
+       })
+     } else {
+       // id 값이 없을 때
+       res.render('view', {topics:files, title:'Welcome', description:'Hello, JavaScript for server.'});
+     }
+   })
+ });*/
+
+ app.get('/topic/new', function(req, res){
+   fs.readdir('data', function(err, files){//디렉토리를 읽는다
+     if(err){
+       console.log(err);
+       res.status(500).send('err!!!!!!!');
+     }
+     res.render('new', {topics:files});//확장자를 뺀 템플릿 파일이름
+   });
+ });
+
+//send함수가 실행되면 그 다음은 실행되지 않음.
+ app.post('/topic', function(req, res){
+   var title = req.body.title;
+   var description = req.body.description;
+   fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      //예를 들면 data2처럼 없는 폴더에 저장한다면 에러.
+      //status(500)은 에러낫다고 컴퓨터에게 알리는 방법.
+       console.log(err);
+       res.status(500).send('Internal Server Error');
+     }
+     res.redirect('/topic/'+title);//redirect를 통해 특정 페이지로 보내버림
+   });
+ });
+
+ app.get(['/topic', '/topic/:id'], function(req, res){
+  fs.readdir('data', function(err, files){//디렉토리를 읽는다
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+
+    if(id){
+      //id값이 있을 때
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      });
+    }
+    else{
+      //id값이 없을 때
+      res.render('view', {topics:files, title:'Welcome', description:'hello javascript'});//첫 번째 인자는 확장자를 뺀 템플릿 파일이름
+    }
+
+  });
+});
+
+ app.listen(3000, function(){
+   console.log('Connected, 3000 port!');
+ })
